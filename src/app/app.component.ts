303dbc66c5adde19d2563450767da88b71d746b5
Frédéric TRAN <frederictran@hotmail.fr>
2024-02-24 18:11:54 +0100

    feat: Working Login/Logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
-import { NgbDropdownModule, NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import {
+  NgbDropdownModule,
+  NgbModule,
+  NgbNavModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
-import { API_URL } from '../config';
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, NgbNavModule, NgbDropdownModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NgbModule,
+    NgbNavModule,
+    NgbDropdownModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'PRLiveTFT-frontend';
+  apiService = new ApiService();
+  logged!: boolean;
+
+  ngOnInit(): void {
+    this.apiService.getWhoAmI().then((response) => {
+      if (response.status === 200) {
+        this.logged = true;
+      } else {
+        this.logged = false;
+      }
+    });
+  }
 }
