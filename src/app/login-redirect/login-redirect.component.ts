b470b2a1c8b375619da76e2092fe9961c5dc5a20
Frédéric TRAN <frederictran@hotmail.fr>
2024-02-25 15:54:23 +0100

    feat: Working /login redirection

--- src/app/login-redirect/login-redirect.component.ts
+++ src/app/login-redirect/login-redirect.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-login-redirect',
   standalone: true,
   imports: [],
   templateUrl: './login-redirect.component.html',
-  styleUrl: './login-redirect.component.css'
+  styleUrl: './login-redirect.component.css',
 })
 export class LoginRedirectComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
+  ngOnInit(): void {
+    this.document.location.href =
+      'http://localhost:5000/api/auth/discord/login';
+  }
 }
